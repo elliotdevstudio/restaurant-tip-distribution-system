9f6f619a6043a9ed23295a11b03bcb2510f83a34
ElliotHarrison <ellioth1029@gmail.com>
2026-01-21 00:10:54 -0600

    adds alphabetical sorting by last name in reports for staff member drop down menu

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -388,9 +388,11 @@ export default function ShiftReportsPage() {
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
               >
                 <option value="">Choose a staff member...</option>
-                {staffMembers.map(member => (
-                  <option key={member.id} value={member.id}>
-                    {member.lastName}, {member.firstName}
+                {[...staffMembers]
+                  .sort((a, b) => a.lastName.localeCompare(b.lastName))
+                  .map(member => (
+                    <option key={member.id} value={member.id}>
+                      {member.lastName}, {member.firstName}
                   </option>
                 ))}
               </select>
